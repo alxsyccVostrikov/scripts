55a2575643614ab80a896f011ffebc068ed52675
pos1620 <pos1620@outlook.com>
2019-02-14 11:25:51 -0300

    updt updtPRJ.sh scpt file

--- updtPRJ.sh
+++ updtPRJ.sh
@@ -1,13 +1,50 @@
 folders=(`ls -p ../ | egrep /`)
-scpts=(`ls ../*.sh`)
+scpts=(`ls *.sh`)
+scpts2=(`ls ../*.sh`)
+: '
 for d in ${folders[@]}; do
-#echo	(cd .. ; sh/scp.sh updting $d folder)
 
-$(../git add $d; git commit -m updting $d folder; git push -f)
+echo "im in $d`echo $?`"
+	(
+	cd /c/gitlab2/lai
+	git add $d
+	echo "$d`echo $?`"
+	git commit -m "updt $d folder"
+	if [ $? -eq 0 ]; then
+	git push -f
+	else
+	echo "$d`echo $?`"
+	fi
+	)
+done 
+'
 
+for scpt in ${scpts[@]}; do
+echo "im in $scpt/`echo $?`"
+	(
+	cd /c/gitlab2/lai/scripts
+	git add $scpt
+	echo "$scpt`echo $?`"
+	git commit -m "updt $scpt scpt file"
+	if [ $? -eq 0 ]; then
+	git push -f
+	else
+	echo "$scpt`echo $?`"
+	fi
+	)
+done 
+
+
+for scpt2 in ${scpts2[@]}; do
+echo "im in lai/$scpt2/`echo $?`"
+	cd /c/gitlab2/lai/
+	git add $scpt2
+	echo "$scpt2`echo $?`"
+	git commit -m "updt $scpt2 scpt file"
+	if [ $? -eq 0 ]; then
+	git push -f
+	else
+	echo "$scpt2`echo $?`"
+	fi
 done;
-#for s in ${scpts[@]}; do
-#echo	(cd ../ ; sh/scp.sh updting $s file)
-#(cd ../ ; sh/scp.sh updting $s file)
-#done;
 
