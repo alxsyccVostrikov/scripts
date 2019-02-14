0314bb5af4d0cd4e8bf02b03928cbe0eb8bd8397
pos1620 <pos1620@outlook.com>
2019-02-14 09:25:52 -0300

    written by alxsy,updting updtPRJ

--- updtPRJ.sh
+++ updtPRJ.sh
@@ -1,4 +1,13 @@
 folders=(`ls -p ../ | egrep /`)
+scpts=(`ls ../*.sh`)
 for d in ${folders[@]}; do
-	sh ../scp.sh updting $d
+#echo	(cd .. ; sh/scp.sh updting $d folder)
+
+$(../git add $d; git commit -m updting $d folder; git push -f)
+
 done;
+#for s in ${scpts[@]}; do
+#echo	(cd ../ ; sh/scp.sh updting $s file)
+#(cd ../ ; sh/scp.sh updting $s file)
+#done;
+
