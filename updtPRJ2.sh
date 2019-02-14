963e798cb895f0e0999d4d300b1dfd0bef5b16f1
pos1620 <pos1620@outlook.com>
2019-02-14 18:34:04 -0300

    updt updtPRJ2.sh scpt file

--- updtPRJ2.sh
+++ updtPRJ2.sh
@@ -1,113 +1,66 @@
 FolderPath=/c/gitlab2/lai
 folders=(`ls -p ../ | egrep /`)
 scpts=(`ls *.sh`)
-scpts2=(`(cd ..;ls *.sh)`)
+scpts2=(`cd ..; ls *.sh`)
 
 
-ProcessorFile(){
+
+
+#files in scritp folder
+ProcessorScriptFile(){
+for scpt in ${scpts[@]}; do
+echo "im in $FolderPath/scripts/$scpt:`echo $?`"
+	(
+	cd $FolderPath/scripts
+	git add $scpt
+	echo "$FolderPath/$scpt:`echo $?`"
+	git commit -m "updt $scpt scpt file"
+	if [ $? -eq 0 ]; then
+	git push -f
+	else
+	echo "$FolderPath/$scpt:`echo $?`"
+	fi
+	)
+done 
+}
+
+#folder in lai
+ProcessorLaiFolder(){
+for d in ${folders[@]}; do
+echo "im in $FolderPath/$d`echo $?`"
+	(
+	cd $FolderPath/
+	git add $d
+	echo "$d`echo $?`"
+	git commit -m "updt $d folder"
+	if [ $? -eq 0 ]; then
+			ProcessorScriptFile
+		git push -f
+	else
+		echo "$d`echo $?`"
+	fi
+	)
+done 
+}
+
+#files in lai
+ProcessorLaiFile(){
 for scpt2 in ${scpts2[@]}; do
 echo "im in $FolderPath/$scpt2:`echo $?`"
-	cd $FolderPath
-	git add $scpt2
-	echo "$scpt2:`echo $?`"
-	git commit -m "updt $scpt2 scpt file"
+	(
+	cd $FolderPath/
+	git add ${scpt2:3}
+	#echo "$scpt2:`echo $?`"
+	git commit -m "updt ${scpt2} scpt file"
 	if [ $? -eq 0 ]; then
 	git push -f
 	else
-	echo "$scpt2:`echo $?`"
-	fi
-done;	
-}
-
-
-ProcessorFolder(){
-for d in ${folders[@]}; do
-
-echo "im in $d`echo $?`"
-	(
-	cd $FolderPath
-	git add $d
-	echo "$d`echo $?`"
-	git commit -m "updt $d folder"
-	if [ $? -eq 0 ]; then
-		if [ $d == "scripts" ]; then
-		for scpt in ${scpts[@]}; do
-echo "im in $scpt/`echo $?`"
-	(
-	cd /c/gitlab2/lai/scripts
-	git add $scpt
-	echo "$scpt`echo $?`"
-	git commit -m "updt $scpt scpt file"
-	if [ $? -eq 0 ]; then
-	git push -f
-	else
-	echo "$scpt/`echo $?`"
+	echo "${scpt2}:`echo $?`"
 	fi
 	)
-done
-
-fi
-	git push -f
-	else
-	echo "$d`echo $?`"
-	fi
-	)
-done 
-}
-ProcessorFile
-ProcessorFolder
-
-
-<<CommentFolderPath
-for d in ${folders[@]}; do
-
-echo "im in $d`echo $?`"
-	(
-	cd /c/gitlab2/lai
-	git add $d
-	echo "$d`echo $?`"
-	git commit -m "updt $d folder"
-	if [ $? -eq 0 ]; then
-		if [ $d == "scripts" ]; then
-		(cd $d;git add $scpt;git commit -m "updt $scpt scpt file";git push -f)
-		fi
-	git push -f
-	else
-	echo "$d`echo $?`"
-	fi
-	)
-done 
-CommentFolderPath
-
-: '
-for scpt in ${scpts[@]}; do
-echo "im in $scpt/`echo $?`"
-	(
-	cd /c/gitlab2/lai/scripts
-	git add $scpt
-	echo "$scpt`echo $?`"
-	git commit -m "updt $scpt scpt file"
-	if [ $? -eq 0 ]; then
-	git push -f
-	else
-	echo "$scpt`echo $?`"
-	fi
-	)
-done 
-'
-<<CommentFilePath
-for scpt2 in ${scpts2[@]}; do
-echo "im in lai/$scpt2:`echo $?`"
-	cd /c/gitlab2/lai/
-	git add $scpt2
-	echo "$scpt2:`echo $?`"
-	git commit -m "updt $scpt2 scpt file"
-	if [ $? -eq 0 ]; then
-	git push -f
-	else
-	echo "$scpt2:`echo $?`"
-	fi
 done;
-CommentFilePath
+}
 
-
+ProcessorLaiFolder
+#ProcessorScriptFile
+ProcessorLaiFile	
