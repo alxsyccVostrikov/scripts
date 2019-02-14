e6af688df7e12f332cd3dac61311af6ef642a503
pos1620 <pos1620@outlook.com>
2019-02-14 19:37:18 -0300

    updt updtPRJ2.sh scpt file,qui, 14 de fev de 2019 19:37:18

--- updtPRJ2.sh
+++ updtPRJ2.sh
@@ -4,28 +4,6 @@ scpts=(`ls *.sh`)
 scpts2=(`cd ..; ls *.sh`)
 
 
-
-ProcessorLaiFolder(){
-#files in scritp folder
-for scpt in ${scpts[@]}; do
-echo "im in $FolderPath/scripts/$scpt:`echo $?`"
-	(
-	cd $FolderPath/scripts
-	git add $scpt
-	echo "$FolderPath/$scpt:`echo $?`"
-	git commit -m "updt $scpt scpt file,`date`"
-	if [ $? -eq 0 ]; then
-		if [ $d == "scripts" ]; then
-		ProcessorScriptFile
-			fi
-
-	git push -f
-	else
-	echo "$FolderPath/$scpt:`echo $?`"
-	fi
-	)
-done 
-}
 #folder in lai
 ProcessorScriptFile(){
 for d in ${folders[@]}; do
@@ -44,6 +22,29 @@ echo "im in $FolderPath/$d`echo $?`"
 	)
 done 
 }
+
+#files in scritp folder
+ProcessorLaiFolder(){
+for scpt in ${scpts[@]}; do
+echo "im in $FolderPath/scripts/$scpt:`echo $?`"
+	(
+	cd $FolderPath/scripts
+	git add $scpt
+	echo "$FolderPath/$scpt:`echo $?`"
+	git commit -m "updt $scpt scpt file,`date`"
+	if [ $? -eq 0 ]; then
+		if [ $d = "scripts" ]; then
+		ProcessorScriptFile
+			fi
+
+	git push -f
+	else
+	echo "$FolderPath/$scpt:`echo $?`"
+	fi
+	)
+done 
+}
+
 #files in lai
 ProcessorLaiFile(){
 for scpt2 in ${scpts2[@]}; do
