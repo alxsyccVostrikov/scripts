15c87d7b0856100afd7711f0cc46abfdb13ff455
pos1620 <pos1620@outlook.com>
2019-02-14 11:28:00 -0300

    updt updtPRJ.sh scpt file

--- updtPRJ.sh
+++ updtPRJ.sh
@@ -36,15 +36,17 @@ done
 
 
 for scpt2 in ${scpts2[@]}; do
-echo "im in lai/$scpt2/`echo $?`"
+echo "im in lai/`echo $?`"
+	(
 	cd /c/gitlab2/lai/
 	git add $scpt2
-	echo "$scpt2`echo $?`"
+	echo "$scpt2:`echo $?`"
 	git commit -m "updt $scpt2 scpt file"
 	if [ $? -eq 0 ]; then
 	git push -f
 	else
 	echo "$scpt2`echo $?`"
 	fi
+	)
 done;
 
